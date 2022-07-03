f5f007a21021ed862ff5ced3938be71558d647a0
Matheus Novais <matheusnovaisz@gmail.com>
2022-07-03 15:34:02 -0300

    🔐 change app port variable name to match heroku

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  await app.listen(process.env.APP_PORT || 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
